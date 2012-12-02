88cc0daa8f159cc95376d3336d74796d2abdc85e
Gabe Charleboix <gcharleboix@hotmail.com>
2012-12-02 10:56:53 -0600

    Tried to get http proxies working, java's being a fucking dick right now though.

---
+++ src/net/sini/nkvoter/io/HttpProxySocket.java
@@ -0,0 +1,99 @@
+package net.sini.nkvoter.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.SocketException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ *
+ * @author bla
+ */
+public class HttpProxySocket extends java.net.Socket{
+    /**
+     * Various states of this socket.
+     */
+    private boolean created = false;
+    private boolean bound = false;
+    private boolean connected = false;
+    private boolean closed = false;
+    private Object closeLock = new Object();
+    private boolean shutIn = false;
+    private boolean shutOut = false;
+
+    private Proxy prx;
+    private URLConnection conn;
+
+    public HttpProxySocket(Proxy proxy) {
+        if (proxy != null && proxy.type() == Proxy.Type.HTTP) {
+            prx = proxy;
+        } else {
+            throw new IllegalArgumentException("Invalid Proxy");
+        }
+    }
+
+    public void connect(SocketAddress endpoint) throws IOException {
+        connect(endpoint, 0);
+    }
+
+    public void connect(InetSocketAddress endpoint, int timeout) throws IOException {
+        if (endpoint == null)
+            throw new IllegalArgumentException("connect: The address can't be null");
+
+        if (timeout < 0)
+          throw new IllegalArgumentException("connect: timeout can't be negative");
+
+        if (isClosed())
+            throw new SocketException("Socket is closed");
+
+        if (!(endpoint instanceof InetSocketAddress))
+            throw new IllegalArgumentException("Unsupported address type");
+
+       URL url = new URL("http://" + endpoint.getAddress().getHostName());
+       
+       try{
+           conn = url.openConnection(prx);
+           
+           conn.setDoInput(true);
+           conn.setDoOutput(true);
+           
+           connected = true;
+           bound = true;
+       } catch(SocketException e)
+       {
+           throw new IOException("Shit happened with ur http proxy dude");
+       }
+    }
+
+    public InputStream getInputStream() throws IOException {
+        if (isClosed())
+            throw new SocketException("Socket is closed");
+        if (!connected)
+            throw new SocketException("Socket is not connected");
+        if (isInputShutdown())
+            throw new SocketException("Socket input is shutdown");
+
+        InputStream is = conn.getInputStream();
+
+        return is;
+    }
+
+    public OutputStream getOutputStream() throws IOException {
+        if (isClosed())
+            throw new SocketException("Socket is closed");
+        if (!connected)
+            throw new SocketException("Socket is not connected");
+        if (isOutputShutdown())
+            throw new SocketException("Socket output is shutdown");
+
+        OutputStream os = conn.getOutputStream();
+        return os;
+    }
+}
--- src/net/sini/nkvoter/io/impl/ProxySocketFactory.java
+++ src/net/sini/nkvoter/io/impl/ProxySocketFactory.java
@@ -8,27 +8,61 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.Socket;
+import java.net.SocketException;
+import net.sini.nkvoter.io.HttpProxySocket;
 import net.sini.nkvoter.io.SocketFactory;
 
 /**
  *
- * @author Gabe
+ * @author bla
  */
 public class ProxySocketFactory  extends SocketFactory{
     	private InetSocketAddress proxyAddress;
+        private boolean httpProxy;
 
     public ProxySocketFactory(InetSocketAddress proxyAddress)
     {
 	this.proxyAddress = proxyAddress;
+        httpProxy = false;
     }
 
     @Override
     public Socket createSocket(InetSocketAddress address) throws IOException {
-		Proxy proxy = new Proxy(Proxy.Type.SOCKS, proxyAddress);
-		Socket socket = new Socket(proxy);
-		socket.setSoTimeout(15000);
-		
-		socket.connect(address);
-        return socket;
+        Proxy proxy;
+        Socket socket;
+        
+        if(!httpProxy){
+            try{
+                proxy = new Proxy(Proxy.Type.SOCKS, proxyAddress);
+                socket = new Socket(proxy);
+                socket.setSoTimeout(15000);
+
+                socket.connect(address);
+                return socket;
+            } catch(SocketException se)
+            {
+                System.out.println("Trying http proxy");
+                try{
+                    socket = httpConnect(address);
+                    httpProxy = true;
+                    return socket;
+                } catch(IOException e)
+                {
+                    throw e;
+                }
+            }
+        }
+        
+        System.out.println("The whole thing's fucked.");
+        return new Socket();
+    }
+    
+    private Socket httpConnect(InetSocketAddress address) throws IOException
+    {
+        HttpProxySocket hSock =  new HttpProxySocket(new Proxy(Proxy.Type.HTTP, proxyAddress));
+        
+        hSock.connect(address, 15000);
+        
+        return hSock;
     }
 }
