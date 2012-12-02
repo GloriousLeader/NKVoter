9bb5120cedd18bece5c45235924716693f3ebede
Onon <vnxlpdaa@sharklasers.com>
2012-12-02 12:57:47 +0100

    Cleaned of stupidity

--- src/net/sini/nkvoter/core/PollDaddyVoteStrategyFactory.java
+++ src/net/sini/nkvoter/core/PollDaddyVoteStrategyFactory.java
@@ -66,74 +66,74 @@ public final class PollDaddyVoteStrategyFactory extends VoteStrategyFactory {
         String[] Undoc_strings = new String[3];
         Undoc_strings[0] = "Undocumented";
         Undoc_strings[1] = "/n/e1386f1d203929ebe8c0202ab9b529bf/6685607?";
-        Undoc_strings[2] = "/vote-js.php?p=6685607&b=1&a=30279757,&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129191%2C00.html&n=";
+        Undoc_strings[2] = "/vote-js.php?p=6685607&b=1&a=30279757&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129191%2C00.html&n=";
         candidates.put("Undoc", Undoc_strings);
         
         String[] Stephen_strings = new String[3];
         Stephen_strings[0] = "Stephen";
         Stephen_strings[1] = "/n/45a54d304efd9783196e1db13da69194/6685714?";
-        Stephen_strings[2] = "/vote-js.php?p=6685714&b=1&a=30280243,&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129212%2C00.html&n=";
+        Stephen_strings[2] = "/vote-js.php?p=6685714&b=1&a=30280243&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129212%2C00.html&n=";
         candidates.put("Stephen", Stephen_strings);
         
         String[] Gabrielle_strings = new String[3];
         Gabrielle_strings[0] = "Gabrielle";
         Gabrielle_strings[1] = "/n/77beeeb2bb776cc255eb4e3fac1cb624/6685689?";
-        Gabrielle_strings[2] = "/vote-js.php?p=6685689&b=1&a=30280171,&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129203%2C00.html&n=";
+        Gabrielle_strings[2] = "/vote-js.php?p=6685689&b=1&a=30280171&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129203%2C00.html&n=";
         candidates.put("Gabrielle", Gabrielle_strings);
         
         String[] Aung_strings = new String[3];
-        Gabrielle_strings[0] = "Aung";
-        Gabrielle_strings[1] = "/n/f9ff5c88db083cd08a595210a3b21335/6685635?";
-        Gabrielle_strings[2] = "/vote-js.php?p=6685635&b=1&a=30279879,&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129196%2C00.html&n=";
+        Aung_strings[0] = "Aung";
+        Aung_strings[1] = "/n/f9ff5c88db083cd08a595210a3b21335/6685635?";
+        Aung_strings[2] = "/vote-js.php?p=6685635&b=1&a=30279879&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129196%2C00.html&n=";
 
         candidates.put("Aung", Aung_strings);
         
         String[] Christie_strings = new String[3];
-        Gabrielle_strings[0] = "Christie";
-        Gabrielle_strings[1] = "/n/a1494915370b2c2fa356b26a34428e7b/6685577?";
-        Gabrielle_strings[2] = "/vote-js.php?p=6685577&b=1&a=30279643,&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129182%2C00.html&n=";
+        Christie_strings[0] = "Christie";
+        Christie_strings[1] = "/n/a1494915370b2c2fa356b26a34428e7b/6685577?";
+        Christie_strings[2] = "/vote-js.php?p=6685577&b=1&a=30279643&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129182%2C00.html&n=";
         candidates.put("Christie", Christie_strings);
         
         String[] Hillary_strings = new String[3];
-        Gabrielle_strings[0] = "Hillary";
-        Gabrielle_strings[1] = "/n/1893654f84b7a11625b092f4779375c2/6685580?";
-        Gabrielle_strings[2] = "/vote-js.php?p=6685580&b=1&a=30279653,&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129184%2C00.html&n=";
+        Hillary_strings[0] = "Hillary";
+        Hillary_strings[1] = "/n/1893654f84b7a11625b092f4779375c2/6685580?";
+        Hillary_strings[2] = "/vote-js.php?p=6685580&b=1&a=30279653&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129184%2C00.html&n=";
         candidates.put("Hillary", Hillary_strings);
         
         String[] AiWeiwei_strings = new String[3];
-        Gabrielle_strings[0] = "AiWeiwei";
-        Gabrielle_strings[1] = "/n/274f014fe83e2dec59c9894b0ba6afa2/6685620?";
-        Gabrielle_strings[2] = "/vote-js.php?p=6685620&b=1&a=30279821,&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129193%2C00.html&n=";
+        AiWeiwei_strings[0] = "AiWeiwei";
+        AiWeiwei_strings[1] = "/n/274f014fe83e2dec59c9894b0ba6afa2/6685620?";
+        AiWeiwei_strings[2] = "/vote-js.php?p=6685620&b=1&a=30279821&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129193%2C00.html&n=";
         candidates.put("AiWeiwei", AiWeiwei_strings);
         
         String[] Morsi_strings = new String[3];
-        Gabrielle_strings[0] = "Morsi";
-        Gabrielle_strings[1] = "/n/f5feb0194a8f56d8ffa40f1ddd6cb0af/6685628?";
-        Gabrielle_strings[2] = "/vote-js.php?p=6685628&b=1&a=30279854,&o=&va=16&cookie=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129194%2C00.html&n=";
+        Morsi_strings[0] = "Morsi";
+        Morsi_strings[1] = "/n/f5feb0194a8f56d8ffa40f1ddd6cb0af/6685628?";
+        Morsi_strings[2] = "/vote-js.php?p=6685628&b=1&a=30279854&o=&va=16&cookie=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129194%2C00.html&n=";
         candidates.put("Morsi", Morsi_strings);
         
         String[] Assad_strings = new String[3];
-        Gabrielle_strings[0] = "Assad";
-        Gabrielle_strings[1] = "/n/cd8babebc20d724de7f9326b90871c31/6685629?";
-        Gabrielle_strings[2] = "/vote-js.php?p=6685629&b=1&a=30279856,&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129195%2C00.html&n=";
+        Assad_strings[0] = "Assad";
+        Assad_strings[1] = "/n/cd8babebc20d724de7f9326b90871c31/6685629?";
+        Assad_strings[2] = "/vote-js.php?p=6685629&b=1&a=30279856&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129195%2C00.html&n=";
         candidates.put("Assad", Assad_strings);
         
         String[] ELJames_strings = new String[3];
-        Gabrielle_strings[0] = "ELJames";
-        Gabrielle_strings[1] = "/n/f25d6b945b051886812e322f2990bacd/6685699?";
-        Gabrielle_strings[2] = "/vote-js.php?p=6685699&b=1&a=30280200,&o=&va=16&cookie=0&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129207%2C00.html&n=";
+        ELJames_strings[0] = "ELJames";
+        ELJames_strings[1] = "/n/f25d6b945b051886812e322f2990bacd/6685699?";
+        ELJames_strings[2] = "/vote-js.php?p=6685699&b=1&a=30280200&o=&va=16&cookie=0&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129207%2C00.html&n=";
         candidates.put("ELJames", ELJames_strings);
         
         String[] Goodell_strings = new String[3];
-        Gabrielle_strings[0] = "Goodell";
-        Gabrielle_strings[1] = "/n/c5b26c7c51ad0c1fe4d82c9c273fb31d/6685666?";
-        Gabrielle_strings[2] = "/vote-js.php?p=6685666&b=1&a=30280073,&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129201%2C00.html&n=";
+        Goodell_strings[0] = "Goodell";
+        Goodell_strings[1] = "/n/c5b26c7c51ad0c1fe4d82c9c273fb31d/6685666?";
+        Goodell_strings[2] = "/vote-js.php?p=6685666&b=1&a=30280073&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129201%2C00.html&n=";
         candidates.put("Goodell", Goodell_strings);
         
         String[] Adelson_strings = new String[3];
-        Gabrielle_strings[0] = "Adelson";
-        Gabrielle_strings[1] = "/n/7a0bb8ade981eb9c2bc955e0587e7180/6685590?";
-        Gabrielle_strings[2] = "/vote-js.php?p=6685590&b=1&a=30279691,&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129186%2C00.html&n=";
+        Adelson_strings[0] = "Adelson";
+        Adelson_strings[1] = "/n/7a0bb8ade981eb9c2bc955e0587e7180/6685590?";
+        Adelson_strings[2] = "/vote-js.php?p=6685590&b=1&a=30279691&o=&va=16&c=1&url=http%3A//www.time.com/time/specials/packages/article/0%2C28804%2C2128881_2128882_2129186%2C00.html&n=";
         candidates.put("Adelson", Adelson_strings);
     }
 }
